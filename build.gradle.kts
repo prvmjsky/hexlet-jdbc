1989e7bce3ae872e1b8138e14f3aad798c37847b
prvmjsky <lukyanovalexey1@gmail.com>
2025-06-01 22:25:55 +0500

    init commit

---
+++ build.gradle.kts
@@ -0,0 +1,22 @@
+plugins {
+    id("java")
+}
+
+group = "hexlet.code"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.11.4"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    compileOnly("org.projectlombok:lombok:1.18.36")
+    annotationProcessor("org.projectlombok:lombok:1.18.36")
+    implementation("com.h2database:h2:2.2.220")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
