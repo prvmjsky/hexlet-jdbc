086bfd9b97d02a5b873ee473fb3c7a1a3f2eee12
prvmjsky <lukyanovalexey1@gmail.com>
2025-06-02 16:10:39 +0500

    add alex with tommy through PreparedStatement

--- src/main/java/io/hexlet/Application.java
+++ src/main/java/io/hexlet/Application.java
@@ -13,9 +13,15 @@ public class Application {
                 statement.execute(sql);
             }
 
-            var sql2 = "INSERT INTO users (username, phone) VALUES ('tommy', '123456789')";
-            try (var statement2 = conn.createStatement()) {
-                statement2.executeUpdate(sql2);
+            var sql2 = "INSERT INTO users (username, phone) VALUES (?, ?)";
+            try (var statement2 = conn.prepareStatement(sql2)) {
+                statement2.setString(1, "tommy");
+                statement2.setString(2, "123456789");
+                statement2.executeUpdate();
+
+                statement2.setString(1, "alex");
+                statement2.setString(2, "333333333");
+                statement2.executeUpdate();
             }
 
             var sql3 = "SELECT * FROM users";
